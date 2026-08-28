d1d01e0e23226a04ea52aebf68b747d381b3cb7a
pentakotabharath25csm-stack <pentakotabharath.25.csm@anits.edu.in>
2026-08-28 10:42:43 +0530

    Add LoopState class for multiplication table and Fibonacci

---
+++ Observation/Task1/code2.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+ 
+
+ //looping statsements of 1st program
+public class LoopState {
+
+    public static void main(String[] args) {
+Scanner m = new Scanner(System.in);
+int n;
+System.out.println("enter the number");
+n = m.nextInt();
+System.out.println("the multipilcation table of given number");
+for(int i = 1; i<= 10; i++){
+    System.out.println(n + "x" + i + " = " + (n*i));
+}        
+   }
+}
+
+/*
+ //LoopState of 2nd problem is numbers display
+ 
+
+public class LoopState {
+
+    public static void main(String[] args) {
+        Scanner N = new Scanner(System.in);
+        int n;
+        System.out.println(" enter the number");
+        n = N.nextInt();
+        System.out.println("Numbers from 1 to n" + (n));
+        for (int i =1; i <= n; i++){
+            System.out.println("");
+        }
+    }
+}*/
+    
+/*
+
+
+ // LoopState of 3rd problem sumof naturalnumbers
+ 
+public class LoopState {
+public static void main(String[] args) {
+    Scanner sn = new Scanner(System.in);
+int n;
+System.out.println("enter the number");
+n = sn.nextInt();
+int sum = 0;
+for (int i = 1;i<=n;i++){
+    sum = sum +i;
+}
+System.out.println("sum = " + sum);
+}
+  
+}
+
+*/
+
+
+ //LoopState 4th problem is fibonacci series
+ */
+public class LoopState {
+public static void main(String[] args) {
+    Scanner f = new Scanner(System.in);
+    int n;
+    System.out.println("enter n");
+    n  = f.nextInt();
+    int a = 0 ;
+    int  b= 1;
+    System.out.println("the fibonacci series");
+for( int i= 1; i<= n;i++){
+    System.out.print(a+"");
+
+int c = a+b;
+a = b;
+b = c;
+}
+}
+}*/
+
+
