6ae85f76d569f2bdd5d24fa527d2921bf225324a
pentakotabharath25csm-stack <pentakotabharath.25.csm@anits.edu.in>
2026-09-11 09:55:47 +0530

    Add files via upload

---
+++ Practice programms/NPE.java
@@ -0,0 +1,9 @@
+public class NPE {
+    public static void main(String[] args) 
+    {
+    String word = null; 
+    int length = word.length();
+   
+    System.out.println("word length is: " + length);
+    }
+}
